4cfb3e2a13829a7557e09efb221157cc35cb63cb
Ericky Vand <erickyvand@gmail.com>
2021-02-24 18:16:25 +0200

    chore(project): add project setup

---
+++ src/tests/app/app.test.js
@@ -0,0 +1,43 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { NOT_FOUND, OK } from 'http-status';
+import server from '../../server';
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('App', () => {
+	it('Should show welcome message on root route', done => {
+		chai
+			.request(server)
+			.get('/')
+			.end((err, res) => {
+				res.body.should.be.an('object');
+				res.body.should.have.property('status');
+				res.body.status.should.be.a('number');
+				res.body.status.should.equal(OK);
+				res.body.should.have.property('message');
+				res.body.message.should.be.a('string');
+				res.body.message.should.equal('One to one chat API');
+			});
+		done();
+	});
+
+	it('Should return not found message when a wrong root has been provided', done => {
+		chai
+			.request(server)
+			.get('/app')
+			.end((err, res) => {
+				res.body.should.be.an('object');
+				res.body.should.have.property('status');
+				res.body.status.should.be.a('number');
+				res.body.status.should.equal(NOT_FOUND);
+				res.body.should.have.property('message');
+				res.body.message.should.be.a('string');
+				res.body.message.should.equal(
+					'Route you are trying to access does not exists'
+				);
+			});
+		done();
+	});
+});
